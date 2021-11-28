e26ff534ada5e6f3fd64e71433904b3a3610795f
fenil29 <fenilkaneria@gmail.com>
2021-11-28 00:47:36 -0500

    add comments

--- src/WebCrawler.java
+++ src/WebCrawler.java
@@ -23,30 +23,39 @@ public class WebCrawler {
 		}
 
 	}
-
+	// crawl website with the given length upto given depth and save it in folder with title as name of file
 	public static void  crawlWebsite(String URL,int crawlingDepth) throws Exception {
 		Set<String> visited = new HashSet<String>();
 		ArrayList<String> links = new ArrayList<String>();
 		ArrayList<Integer> depth = new ArrayList<Integer>();
+		// add current website to crawling queue		
 		links.add(URL);
 		depth.add(0);
 		int currnetDepth = 0;
 		for (int i = 0; i < links.size(); i++) {
+			
+			// exit the loop if the given depth is reached			
 			if(depth.get(i)>crawlingDepth) {
 				break;
 			}
 			String link = links.get(i);
-			
+				
+			// skip the loop if the current link is already visited			
 			if (visited.contains(link)) {
 				continue;
 			}
 			visited.add(link);
 			System.out.println("Crawling Website : " + link);
 			try {
-				Document document = Jsoup.connect(link).get();				
+				// getting the link				
+				Document document = Jsoup.connect(link).get();	
+				
+				// save the content of website in folder				
 				writeToFile(document.title(), document.html());
 				Elements linksOnPage = document.select("a[href]");
 				currnetDepth++;
+				
+				// add all hyperlink to the crawling list 				
 				for (Element page : linksOnPage) {
 					links.add(page.attr("abs:href"));
 					depth.add(currnetDepth);
